57e3efea0d1c131c7441597636fcaf61125bbce3
tabbas651 <thamimrazith_abbasali@comcast.com>
2025-10-28 18:30:14 +0000

    Updated the Plugin code changes

---
+++ plugin/HelloPlugin.cpp
@@ -0,0 +1,81 @@
+#ifndef MODULE_NAME
+#define MODULE_NAME MyHelloPlugin
+#endif
+
+#include <core/core.h>
+#include <plugins/plugins.h>
+#include <thread>
+
+MODULE_NAME_DECLARATION("MyHelloPlugin")
+
+namespace Thunder
+{
+    namespace Plugin
+    {
+        class HelloPlugin : public PluginHost::IPlugin, public PluginHost::JSONRPC
+        {
+            public:
+            HelloPlugin()
+                : PluginHost::JSONRPC()
+            {
+                Register(_T("SayHello"), &HelloPlugin::SayHello, this);
+
+            }
+
+	    ~HelloPlugin() override
+            {
+                Unregister(_T("SayHello"));
+    	    }
+
+
+	    virtual const string Initialize(PluginHost::IShell* service) override
+            {
+                SYSLOG(Logging::Startup, (_T("Initializing TestPlugin")));
+
+                return string();
+            }
+
+            virtual void Deinitialize(PluginHost::IShell* service) override
+            {
+                SYSLOG(Logging::Startup, (_T("Deinitializing TestPlugin")));
+            }
+
+            virtual string Information() const override
+            {
+                return string();
+            }
+
+            // JSON-RPC method implementation
+            uint32_t SayHello(const JsonObject& parameters, JsonObject& response)
+            {
+                SYSLOG(Logging::Startup, (_T("SayHello called")));
+
+                string name = parameters.HasLabel("name") ? parameters["name"].String() : "World";
+
+                response["message"] = "Hello " + name + " from TestPlugin";
+
+                return Core::ERROR_NONE; // 0 = success
+            }
+            
+	    //Build QueryInterface implementation, specifying all possible interfaces to be returned.
+            BEGIN_INTERFACE_MAP(TestPlugin)
+            INTERFACE_ENTRY(PluginHost::IPlugin)
+            INTERFACE_ENTRY(PluginHost::IDispatcher)
+            END_INTERFACE_MAP
+        };
+
+        static Plugin::Metadata<Plugin::HelloPlugin> metadata(
+            // Version (Major, Minor, Patch)
+            1, 0, 0,
+            // Preconditions
+            {},
+            // Terminations
+            {},
+            // Controls
+            {}
+        );
+
+    }
+}
+
+
